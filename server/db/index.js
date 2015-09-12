7f09faac7eaa86eeefb5dcf4c5bd79f58e82e3ae
jingpei <jingjpei@gmail.com>
2015-09-11 17:33:37 -0700

    Created DB and Tables

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,32 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+//Create connection object
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'chat'
+});
+//Connect to database
+connection.connect();
+//Query for all messages
+module.exports = {
+  //Message requests
+  messages: {
+    //Get messages query
+    getMessages: function(){
+      connection.query("SELECT * FROM messages ORDER BY createdAt DESC", function(err, rows, fields){
+        //If there's an error
+        if(err) throw err;
+        console.log(rows);
+        return rows;
+      });
+    }
+  },
+  //User requests
+  users: {
 
+  }
+
+};
--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,39 @@
+DROP DATABASE chat;
+
+
 CREATE DATABASE chat;
 
 USE chat;
 
+
+/*Messages*/
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  userId INT NOT NULL,
+  text VARCHAR(255),
+  createdAt DATE,
+  roomName VARCHAR(255)
 );
 
-/* Create other tables and define schemas for them here! */
+/* Create other tables and define schemas for them here! */   
 
+/*All users*/
+CREATE TABLE users(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  userName VARCHAR(255) NOT NULL
+);
 
-
+/*Friends*/
+CREATE TABLE friends(
+  idA INT NOT NULL,
+  idB INT NOT NULL
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+/*
+SELECT friendsOfOne.idB AS friends From ((select idB from u2u where idA=1 and idB<>1) union (select idA from u2u where idB = 1 and idA <> 1)) AS friendsOfOne;;;;;
+*/
